b246a8c79c55e423024f3b58b71dbb067c87edf7
Mohamed Ashraf <mohamedashraf171998@gmail.com>
2025-11-05 15:45:25 +0200

    AddSpecificationDesignPattern

--- Talapat.Repository/Repositories/GenericRepository.cs
+++ Talapat.Repository/Repositories/GenericRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
 using Talabat.Core.IRepositories;
+using Talabat.Core.Specifications;
 using Talapat.Repository.Data;
 
 namespace Talapat.Repository.Repositories
@@ -23,9 +24,24 @@ namespace Talapat.Repository.Repositories
             return await _dbContext.Set<T>().ToListAsync();
         }
 
+      
         public async Task<T?> GetByIdAsync(int id)
         {
             return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllWithSpecAsync(ISpecifications<T> spec)
+        {
+            return await ApplySpecifications(spec).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdWithSpecAsync(ISpecifications<T> spec)
+        {
+            return await ApplySpecifications(spec).AsNoTracking().FirstOrDefaultAsync();
+        }
+        private IQueryable<T> ApplySpecifications(ISpecifications<T> spec)
+        {
+          return SpecificationEvaluator<T>.GetQuery(_dbContext.Set<T>(), spec);
         }
     }
 }
